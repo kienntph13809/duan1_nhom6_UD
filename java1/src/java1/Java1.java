1ec995dad45409b79b6f55eeda3286e472f7259d
kienntph13809 <93783345+kienntph13809@users.noreply.github.com>
2021-11-07 22:37:25 +0700

    Add files via upload

---
+++ java1/src/java1/Java1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java1;
+
+/**
+ *
+ * @author kien5
+ */
+public class Java1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a,b,sum;
+        System.out.println("mời bạn nhập số a: ");
+        
+    }
+    
+}
